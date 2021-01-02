6ec52500c5c3fc69cd9c65f6622209f71b697114
jennuwine <jennherrarte93@gmail.com>
2021-01-02 15:32:47 -0800

    Added event listeners and flip function for the tiles

---
+++ js/script.js
@@ -0,0 +1,86 @@
+
+
+// NOTE Variables // 
+
+const userHuman = 'human'
+const computerUser ='computer';
+
+let $compuerScore = 0; 
+let $userScore = 0; 
+
+// NOTE JQuery // 
+$('#user').append($userScore)
+$('#computer').append($compuerScore)
+
+
+
+// NOTE JS //
+
+
+
+
+// NOTE EVENT LISTENERS //
+
+$('#card1').click(function () {
+    console.log('jenn')
+})
+
+$('#card2').click(function () {
+    console.log('jenn')
+})
+
+
+
+$('#card3').click(function () {
+    console.log('jenn')
+})
+
+$('#card4').click(function () {
+    console.log('jenn')
+})
+
+
+
+$('#card5').click(function () {
+    console.log('jenn')
+})
+
+$('#card6').click(function () {
+    console.log('jenn')
+})
+
+
+
+$('#card7').click(function () {
+    console.log('jenn')
+})
+
+$('#card8').click(function () {
+    console.log('jenn')
+})
+
+$('#card9').click(function () {
+    console.log('jenn')
+})
+
+$('#card10').click(function () {
+    console.log('jenn')
+})
+
+$('#card11').click(function () {
+    console.log('jenn')
+})
+
+$('#card12').click(function () {
+    console.log('jenn')
+})
+
+
+// user one goes first
+// if click one and click two equals a winning pair = counter up 
+// another turn 
+
+
+// function(click1, click2) {
+
+// }
