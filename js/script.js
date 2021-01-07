0638ca21e328ebbcc2ffa47cce1b887ff3688a2b
jennuwine <jennherrarte93@gmail.com>
2021-01-07 14:41:29 -0800

    added shuffle function

--- js/script.js
+++ js/script.js
@@ -142,3 +142,10 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 
 
 
+(function shuffle() {
+  cards.forEach(card => {
+  let randomPos = Math.floor(Math.random() * 12);
+  card.style.order = randomPos
+  });
+})();
+
