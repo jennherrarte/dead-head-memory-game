91ba6c384a979e0c85210b6f585a30eedcf88eeb
jennuwine <jennherrarte93@gmail.com>
2021-01-08 10:34:32 -0800

    organized js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-
-
-const cards = document.querySelectorAll('.memory-card');
-
-
+// NOTE VARIABLES //
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -11,121 +7,87 @@ let userScore = 0
 let compScore = 0
 let $userScore = $('.userScore').text(userScore)
 let $compScore = $('.compScore').text(compScore)
-
-
 let player1 = false
 let player2 = false
+const cards = document.querySelectorAll('.memory-card');
 
+cards.forEach(card => card.addEventListener('click', flipCard));
 
+// NOTE FUNCTIONS //
 
+// NOTE COMPUTER SELECTIONS //
 
 function compflipCard() {
-    if (lockBoard) return;
-    player2 = true
-    player1 = false
-    firstCard = cards[Math.floor(Math.random() * $('.memory-card').length)]
-    secondCard = cards[Math.floor(Math.random() * $('.memory-card').length)]
-
-    if (secondCard === firstCard || firstCard.classList.contains('flip') || secondCard.classList.contains('flip') ) {
-        compflipCard() 
-        return;
-    }
-
-   else if ((secondCard != firstCard) &&  (!firstCard.classList.contains('flip')) && (!secondCard.classList.contains('flip'))) {
-  
-        firstCard.classList.add('flip')
-        secondCard.classList.add('flip')
-        checkForMatch(firstCard, secondCard);
-    
-}
-
-
-
-}
-
-
-
-function flipCard() {
-    player1 = true
-    player2 = false
   if (lockBoard) return;
-  if (this === firstCard) return;
+  player2 = true;
+  player1 = false;
+  firstCard = cards[Math.floor(Math.random() * $('.memory-card').length)];
+  secondCard = cards[Math.floor(Math.random() * $('.memory-card').length)];
 
-  this.classList.add('flip');
- 
-
-  if (!hasFlippedCard) {
-
-    hasFlippedCard = true;
-    firstCard = this;
-
+  if (secondCard === firstCard || firstCard.classList.contains('flip') || secondCard.classList.contains('flip') ) {
+    compflipCard() 
     return;
   }
-
-
-  secondCard = this;
-
-  checkForMatch(firstCard, secondCard);
-  
-  setTimeout(() => {
-  compflipCard()
-  }, 3000);
-
-  
- 
+  else if ((secondCard != firstCard) &&  (!firstCard.classList.contains('flip')) && (!secondCard.classList.contains('flip'))) {
+    firstCard.classList.add('flip')
+    secondCard.classList.add('flip')
+    checkForMatch(firstCard, secondCard);
+  }
 }
 
+// NOTE USER SELECTIONS //
 
-
+function flipCard() {
+  player1 = true;
+  player2 = false;
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  checkForMatch(firstCard, secondCard);
+  setTimeout(() => {
+    compflipCard()
+  }, 3000);
+}
 
 function checkForMatch(firstCard, secondCard) {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
   isMatch ? disableCards(firstCard, secondCard) : unflipCards(firstCard, secondCard);
-
-
-
-  if(isMatch === true && player1 === true) {
+  if (isMatch === true && player1 === true) {
     userblink_text() 
     userScore = userScore + 1
     $userScore = $('.userScore').text(userScore)
-    
-    
-
   }
 
-  if(isMatch === true && player2 === true) {
+  if (isMatch === true && player2 === true) {
     blink_text()
     compScore = compScore + 1
     $compScore = $('.compScore').text(compScore)
-    
   }
- 
-  if(compScore + userScore === 6) {
 
+  if (compScore + userScore === 6) {
     setTimeout(() => {
       showModal()
       }, 1000);
-    return
+    return;
   }
-
 }
 
-
-
-
-
+// NOTE DISABLE BOARD //
 
 function disableCards(firstCard, secondCard) {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
 }
 
-
-
-
+// NOTE UNFLIP CARDS //
 
 function unflipCards(firstCard, secondCard) {
   lockBoard = true;
@@ -133,23 +95,18 @@ function unflipCards(firstCard, secondCard) {
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1500);
 }
 
-
-
-
+// NOTE RESET BOARD //
 
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-
+// NOTE SHUFFLE CARDS FOR NEW GAME //
 
 (function shuffle() {
   cards.forEach(card => {
@@ -159,63 +116,52 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 })();
 
 
- function blink_text() {
- 
-  for(i=0; i < 2; i++) {
-   
-   $('.compScore').fadeOut(500);
-   $('.compScore').fadeIn(500);
-  setInterval(500);
- 
-  }
- 
- }
+// NOTE SCORE BLINKING //
 
-   
+ function blink_text() {
+  for (i=0; i < 2; i++) {
+    $('.compScore').fadeOut(500);
+    $('.compScore').fadeIn(500);
+    setInterval(500);
+  }
+ }
 
  function userblink_text() {
- 
-  for(i=0; i < 2; i++) {
-
-   $('.userScore').fadeOut(500);
-   $('.userScore').fadeIn(500);
-  setInterval(500);
+  for (i=0; i < 2; i++) {
+      $('.userScore').fadeOut(500);
+      $('.userScore').fadeIn(500);
+    setInterval(500);
   }
  
  }
 
-
-
-
+// NOTE MODAL //
 
 function showModal() {
-  
   $(document).ready(function(){
-    $('#myModal').modal("show")
+    $('#myModal').modal('show')
   });
 
-if(userScore > compScore) {
+  if (userScore > compScore) {
   $('.modal-message').text("You won! That's rad")
-}
-if(userScore === compScore) {
+  }
+  if (userScore === compScore) {
   $('.modal-message').text("It's a tie! That's cool too")
-} if (compScore > userScore) {
+  } if (compScore > userScore) {
   $('.modal-message').text("Better luck next time...")
-}
+  }
 
-$( ".btn-primary").click(function() {
+  $( '.btn-primary').click(function() {
   location.reload();
   userScore = 0
   compScore = 0
   $compScore = $('.compScore').text(compScore)
   $userScore = $('.userScore').text(userScore)
-  
+
   $(document).ready(function(){
-    $('#myModal').modal("hide")
+    $('#myModal').modal('hide')
   });
-
-})
-
+  })
 }
 
 
